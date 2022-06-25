9e99875c839ed3a97562018d2dc66a01570a81ce
MA7AMED <87530097+Mo7amed-MSR@users.noreply.github.com>
2022-06-25 16:16:08 +0200

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,31 +1,15 @@
-const clockBar = document.querySelector(`.clock-bar`);
-const time = document.querySelector(`.clock-bar h1`);
-const date = document.querySelector(`.content h2`);
-const day = document.querySelector(`.content h3`);
-async function get() {
-  const response = await fetch(
-    "http://worldtimeapi.org/api/timezone/Africa/Cairo"
-  );
-  const data = await response.json();
-  time.innerHTML = data.datetime.slice(11, 16);
-  date.innerHTML = data.datetime.slice(0, 10);
+// start var
+let clockBar = document.querySelector(`.clock-bar`)
+let timeDiv = document.querySelector(`.clock-bar h1`)
+let dayDiv = document.querySelector(`.clock-bar h3`)
+let dateDiv = document.querySelector(`.clock-bar h2`)
 
-  if (data.day_of_week > 0 ,data.day_of_week < 2) {
-    day.innerHTML = `MON`
-  }else if (data.day_of_week > 1 ,data.day_of_week < 3) {
-    day.innerHTML = `TUE`
-  }else if (data.day_of_week > 2 ,data.day_of_week < 4) {
-    day.innerHTML = `WED`
-  }else if (data.day_of_week > 3 ,data.day_of_week < 5) {
-    day.innerHTML = `THU`
-  }else if (data.day_of_week >  4 ,data.day_of_week < 6) {
-    day.innerHTML = `FRI`
-  }else if (data.day_of_week > 5 ,data.day_of_week < 7) {
-    day.innerHTML = `SAT`
-  }else{
-    day.innerHTML = `SUN`
-  }
-  console.log(data)
-  console.log(data.day_of_week)
-}
-get();
+let data = Date();
+let timeData = data.slice(16 , 21);
+let dayData = data.slice(0 , 3).toUpperCase();
+let dateData = `${data.slice(11 , 15)}-${data.slice(4 , 7).toUpperCase()}-${data.slice(8 , 10)}`;
+// end var
+
+timeDiv.innerHTML = timeData;
+dayDiv.innerHTML = dayData;
+dateDiv.innerHTML = dateData;
